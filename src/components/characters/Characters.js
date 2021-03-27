737ac3c9de38aa8aab8b1aa37d863f1719ea2810
Valter <vcvijic@gmail.com>
2021-03-27 01:08:07 +0100

    filter characters by status only

--- src/components/characters/Characters.js
+++ src/components/characters/Characters.js
@@ -1,66 +1,62 @@
-import { useEffect, useState } from "react";
-import { useFetch } from "../../useFetch";
+import { Fragment, useEffect, useState } from "react";
+import { useDropdown } from "../../hooks/useDropdown";
+import { useFetch } from "../../hooks/useFetch";
 import "../../styles/Characters.css";
 
 const Characters = () => {
   const [query, setQuery] = useState("characters");
   const [statuses, setStatuses] = useState([]);
-  const [seasons, setSeasons] = useState([]);
-  const [selectedStatus, setSelectedStatus] = useState("");
-  const [selectedSeason, setSelectedSeason] = useState(false);
+  const [characterStatus, StatusDropdown] = useDropdown("", statuses);
 
   // fetch characters
   const baseUrl = "https://www.breakingbadapi.com/api";
-  const url = query && `${baseUrl}/${query}?limit=19&offset=0`;
+  const url = query && `${baseUrl}/${query}?limit=9&offset=0`;
+
   const { status, data, error } = useFetch(url);
   const characters = data;
 
   useEffect(() => {
     // get all character statuses
-    const charStatuses = characters.map((character) => character.status);
-    const statuses = new Set(charStatuses);
+    const getCharacterStatuses = () => {
+      const charStatusesList = characters.map((character) => character.status);
+      const statuses = new Set(charStatusesList);
+      const statusesArr = Array.from(statuses);
 
-    setStatuses(statuses);
+      setStatuses(statusesArr);
+    };
 
-    // get all seasons
-    const charAppearances = characters.map((character) => character.appearance);
-
-    const reducedAppearances = charAppearances.reduce(
-      (accumulator, currentValue) => accumulator.concat(currentValue),
-      []
-    );
-
-    const appearances = new Set(reducedAppearances);
-
-    setSeasons(appearances);
+    getCharacterStatuses();
   }, [characters]);
 
   return (
-    <div className="container">
-      <div className="row filters">
-        <div className="col-3">
-          <select className="form-select" aria-label="Default select example">
-            <option selected>Status</option>
-            <option value="1">One</option>
-            <option value="2">Two</option>
-            <option value="3">Three</option>
-          </select>
+    <Fragment>
+      <div className="row center-md">
+        <div className="col-md-4">
+          <div className="box-row">
+            <p>Status</p>
+            <StatusDropdown />
+          </div>
         </div>
-        <div className="col-3">
-          <select className="form-select" aria-label="Default select example">
-            <option selected>Appearance</option>
-            <option value="1">One</option>
-            <option value="2">Two</option>
-            <option value="3">Three</option>
-          </select>
+        <div className="col-md-8">
+          <div className="box-row">
+            {status === "error" && <div>{error}</div>}
+            {status === "fetching" && <div className="loading"></div>}
+            {status === "fetched" && (
+              <>
+                {characters.length === 0 && (
+                  <div> No characters found! :( </div>
+                )}
+                {characters.map((c) => (
+                  <div className="article" key={c.char_id}>
+                    <p>{c.name}</p>
+                  </div>
+                ))}
+              </>
+            )}
+          </div>
         </div>
       </div>
-      <div className="row">
-        <div className="col-12">
-          <p>Characters go here...</p>
-        </div>
-      </div>
-    </div>
+    </Fragment>
   );
 };
 
