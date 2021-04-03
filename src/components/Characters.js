446916dfa6e654928bb9412062eb0186529db1f7
Valter <vcvijic@gmail.com>
2021-04-03 23:30:38 +0200

    set characters query in useEffect hook

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -4,7 +4,7 @@ import { useFetch } from "../hooks/useFetch";
 import "../styles/Characters.css";
 
 const Characters = () => {
-  const [query, setQuery] = useState("characters");
+  const [query, setQuery] = useState("");
   const [statuses, setStatuses] = useState([]);
   const [characterStatus, StatusDropdown] = useDropdown("", statuses);
 
@@ -16,6 +16,8 @@ const Characters = () => {
   const characters = data;
 
   useEffect(() => {
+    setQuery("characters");
+
     // get all character statuses
     const getCharacterStatuses = () => {
       const charStatusesList = characters.map((character) => character.status);
@@ -31,13 +33,13 @@ const Characters = () => {
   return (
     <Fragment>
       <div className="row center-md">
-        <div className="col-md-4">
+        <div className="col-md-4 filters">
           <div className="box-row">
             <p>Status</p>
             <StatusDropdown />
           </div>
         </div>
-        <div className="col-md-8">
+        <div className="col-md-4">
           <div className="box-row">
             {status === "error" && <div>{error}</div>}
             {status === "fetching" && <div className="loading"></div>}
